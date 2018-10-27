476d677495c0317457f3372dd7ae743fe408b0b1
German Bisurgi <gebisurgi@gmail.com>
2018-10-27 18:40:05 +0200

    updated sql dump with dummy data

--- pons_translator.sql
+++ pons_translator.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 22. Okt 2018 um 09:03
--- Server-Version: 10.1.33-MariaDB
--- PHP-Version: 7.2.6
+-- Generation Time: Oct 27, 2018 at 06:38 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `pons_translator`
+-- Database: `pons_translator`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
@@ -33,10 +33,19 @@ CREATE TABLE `category` (
   `name` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(2, 'Essen'),
+(3, 'Sports'),
+(4, 'Music');
+
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `word`
+-- Table structure for table `word`
 --
 
 CREATE TABLE `word` (
@@ -49,36 +58,51 @@ CREATE TABLE `word` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Indizes der exportierten Tabellen
+-- Dumping data for table `word`
+--
+
+INSERT INTO `word` (`id`, `categoryId`, `count`, `name`, `language`, `translation`) VALUES
+(1, 2, 0, 'Apfel', 'en', 'Apple'),
+(2, 2, 0, 'Karote', 'en', 'Carrot'),
+(3, 2, 0, 'Salat', 'en', 'Salad'),
+(4, 3, 0, 'Fussball', 'en', 'Football'),
+(5, 3, 0, 'Tanzen', 'en', 'To dance'),
+(6, 3, 0, 'Rennen', 'en', 'to run'),
+(7, 4, 0, 'Singen', 'en', 'to sing'),
+(8, 4, 0, 'Guitarre', 'en', 'Guitar'),
+(9, 4, 0, 'Schlagzeug', 'en', 'Drums');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Indizes für die Tabelle `category`
+-- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indizes für die Tabelle `word`
+-- Indexes for table `word`
 --
 ALTER TABLE `word`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT für exportierte Tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT für Tabelle `category`
+-- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT für Tabelle `word`
+-- AUTO_INCREMENT for table `word`
 --
 ALTER TABLE `word`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
